d1703b3f38c904422e7ff0adfd9fb32bd1c215d7
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-08-24 12:08:36 +0100

    Update formula to v1.1.0 with correct SHA256

--- Formula/system-updater.rb
+++ Formula/system-updater.rb
@@ -2,7 +2,7 @@ class SystemUpdater < Formula
   desc "Cross-platform system update script for Linux and macOS"
   homepage "https://github.com/waynelloyd/homebrew-system-updater"
   url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_FROM_STEP_3"
+  sha256 "4b958d44b224afb50ddd09a4a13c02564976251ef53013ee58a0cb5e3403ff58"
   license "MIT"
 
   depends_on "python@3.11"
