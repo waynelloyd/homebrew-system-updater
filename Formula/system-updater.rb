ee327963feb935ee6af81a1c424178b76ee9bb3c
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-08-29 15:31:47 +0100

    tagged a new release

--- Formula/system-updater.rb
+++ Formula/system-updater.rb
@@ -1,7 +1,7 @@
 class SystemUpdater < Formula
   desc "Cross-platform system update script for Linux and macOS"
   homepage "https://github.com/waynelloyd/homebrew-system-updater"
-  url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "dc9d93291d9a54647ea9cfe71a4530919c7555883979825981a77064c5bf4296"
   license "MIT"
 
