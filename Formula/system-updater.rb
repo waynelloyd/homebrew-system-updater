5a226315b594c5913a6926abe280c41d505f8418
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-09-16 20:45:23 +0100

    New release 1.0.1

--- Formula/system-updater.rb
+++ Formula/system-updater.rb
@@ -6,7 +6,7 @@ class SystemUpdater < Formula
   # 2. Get the .tar.gz URL and its SHA256 hash from the new release page.
   # 3. Update the `url` and `sha256` below.
   url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "f43d7fb75bb6cd017335b94745bbe4fd505ab1ff2d5a660f13258196950ce17f"
   license "MIT"
 
   def install
