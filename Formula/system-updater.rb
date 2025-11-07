afaee89947bc9ccfb930142d1c6af5957ab57ac1
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-11-07 18:22:03 +0000

    Added new release v1.0.4 Added new config file support Added tmux plugin manager support

--- Formula/system-updater.rb
+++ Formula/system-updater.rb
@@ -5,8 +5,8 @@ class SystemUpdater < Formula
   # 1. Create a new release tag in the `system-updater` repository (e.g., v1.0.1).
   # 2. Get the .tar.gz URL and its SHA256 hash from the new release page.
   # 3. Update the `url` and `sha256` below.
-  url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "f2ca49e5b55927a088b3ea3e06efe0cc52acf56b1c99959c98e1ff22e4c4fb14"
+  url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "940cf710d87a543cc4b9a452f5435298921167d266a15e78a46c6cc54f30d7ac"
   license "MIT"
 
   def install
