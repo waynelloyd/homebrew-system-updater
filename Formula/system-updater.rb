7fa51ac5cd5ffaa69fd571b5b38bc75cfc707abd
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-11-18 19:57:13 +0000

    New release v1.0.7 Deadlock fix for new progress spinner used for docker-compose pull

--- Formula/system-updater.rb
+++ Formula/system-updater.rb
@@ -5,8 +5,8 @@ class SystemUpdater < Formula
   # 1. Create a new release tag in the `system-updater` repository (e.g., v1.0.1).
   # 2. Get the .tar.gz URL and its SHA256 hash from the new release page.
   # 3. Update the `url` and `sha256` below.
-  url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/waynelloyd/homebrew-system-updater/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b"
   license "MIT"
 
   def install
